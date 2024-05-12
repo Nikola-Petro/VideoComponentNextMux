0275a62653208f16094d72407708229892db90db
Nikola <niuku27@gmail.com>
2024-05-12 19:35:30 +0200

    Video with captions

--- src/app/page.tsx
+++
@@ -1,6 +0,0 @@
-import Video from 'next-video';
-import aaaa from '/videos/aaaa.mkv';
- 
-export default function Page() {
-  return <Video src={aaaa} />;
-}
---
+++ vidtesting/app/page.tsx
@@ -0,0 +1,12 @@
+import MuxPlayer from '@mux/mux-player-react';
+export default function Home() {
+  return <MuxPlayer
+  streamType="on-demand"
+  playbackId="dAUfdFNXLLMrmKI21nCEjLliHEhjcPqm4ZaVyPKgCYo"
+  metadataVideoTitle="Placeholder (optional)"
+  metadataViewerUserId="Placeholder (optional)"
+  primaryColor="#FFFFFF"
+  secondaryColor="#000000"
+/>;
+}
+
